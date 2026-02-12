6d6384aba552fe80f61ee93704e80e7cf9dc2396
caiquemurilo <caiquemurilo@gmail.com>
2026-02-12 03:10:23 -0300

    feat: add profile edit modal functions

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,44 @@ const initialCards = [
 initialCards.forEach(function (card) {
   console.log(card)
 })
+
+const profileEditModal = document.querySelector('#edit-popup')
+const profileEditOpenBtn = document.querySelector('.profile__edit-button')
+const profileEditCloseBtn = profileEditModal.querySelector('.popup__close')
+// const profileEditSaveBtn = profileEditModal.querySelector('.popup__button')
+const profileEditForm = profileEditModal.querySelector('#edit-profile-form')
+const nameInput = profileEditModal.querySelector('.popup__input_type_name')
+const jobInput = profileEditModal.querySelector('.popup__input_type_description')
+const profileTitle = document.querySelector('.profile__title')
+const profileDescription = document.querySelector('.profile__description')
+
+function openModal(modal) {
+  modal.classList.add('popup_is-opened')
+}
+function closeModal(modal) {
+  modal.classList.remove('popup_is-opened')
+}
+function fillProfileForm() {
+  nameInput.value = profileTitle.textContent
+  jobInput.value = profileDescription.textContent
+}
+function handleOpenEditModal() {
+  openModal(profileEditModal)
+  fillProfileForm()
+}
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault()
+  profileTitle.textContent = nameInput.value
+  profileDescription.textContent = jobInput.value
+  closeModal(profileEditModal)
+}
+
+profileEditOpenBtn.addEventListener('click', function () {
+  handleOpenEditModal()
+})
+
+profileEditCloseBtn.addEventListener('click', function () {
+  closeModal(profileEditModal)
+})
+
+profileEditForm.addEventListener('submit', handleProfileFormSubmit)
